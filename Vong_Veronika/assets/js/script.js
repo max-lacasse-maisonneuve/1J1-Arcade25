4ada017d9daed97445e4b3a2f859dc160d4e0b88
Maxime <mlacassegermain@cmaisonneuve.qc.ca>
2025-12-16 20:10:29 -0500

    ajustement clic

--- Vong_Veronika/assets/js/script.js
+++ Vong_Veronika/assets/js/script.js
@@ -9,12 +9,12 @@ let nLargeur = oCanvasHTML.width;
 let sEtatJeu = "intro"; // intro, game, end
 
 let oBoutonDemarrer = {
-  x: nLargeur / 2 - 100,
-  y: nHauteur - 100,
-  largeur: 200,
-  hauteur: 50,
-  texte: "DÉMARRER",
-  teinte: 0,
+    x: nLargeur / 2 - 100,
+    y: nHauteur - 100,
+    largeur: 200,
+    hauteur: 50,
+    texte: "DÉMARRER",
+    teinte: 0,
 };
 
 let oImageBG = new Image();
@@ -30,12 +30,12 @@ let nVie = 5;
 let nPointage = 0;
 
 let oImageBGReponse = {
-  x: 0,
-  y: 0,
-  largeur: 100,
-  hauteur: 100,
-  image: new Image(),
-  src: "assets/images/temp-planche.png",
+    x: 0,
+    y: 0,
+    largeur: 100,
+    hauteur: 100,
+    image: new Image(),
+    src: "assets/images/temp-planche.png",
 };
 
 let oBoutonsReponse = []; //buttons of answers
@@ -50,263 +50,231 @@ oSonMusique.volume = 0.6;
 //------------------FONCTIONS INITIALISATION JEU------------------------
 
 function initialiser() {
-  setInterval(boucleJeu, 1000 / 60);
-  window.addEventListener("click", ClicCanvas);
-  window.addEventListener("keydown", toucheEnfoncee);
-  window.addEventListener("keyup", toucheRelachee);
+    setInterval(boucleJeu, 1000 / 60);
+    window.addEventListener("click", ClicCanvas);
+    window.addEventListener("keydown", toucheEnfoncee);
+    window.addEventListener("keyup", toucheRelachee);
 
-  oSonMusique.currentTime = 0; //redemarrer le son
+    oSonMusique.currentTime = 0; //redemarrer le son
 }
 
 function toucheEnfoncee(evenement) {
-  touche = evenement.key;
+    touche = evenement.key;
 }
 function toucheRelachee(evenement) {
-  touche = null;
+    touche = null;
 }
 
 //--------------boucle de jeu
 function boucleJeu() {
-  oContexte.clearRect(0, 0, oCanvasHTML.width, oCanvasHTML.height);
-  if (sEtatJeu == "intro") {
-    afficherIntro();
-  } else if (sEtatJeu == "jeu") {
-    afficherJeu();
-  } else if (sEtatJeu == "fin") {
-    afficherFin();
-  } else {
-    console.error("Erreur: état du jeu inconnu");
-  }
+    oContexte.clearRect(0, 0, oCanvasHTML.width, oCanvasHTML.height);
+    if (sEtatJeu == "intro") {
+        afficherIntro();
+    } else if (sEtatJeu == "jeu") {
+        afficherJeu();
+    } else if (sEtatJeu == "fin") {
+        afficherFin();
+    } else {
+        console.error("Erreur: état du jeu inconnu");
+    }
 }
 
 //-------ecouteur evenement
 function ClicCanvas(evenement) {
-  let curseurX = evenement.offsetX;
-  let curseurY = evenement.offsetY;
+    let curseurX = evenement.offsetX;
+    let curseurY = evenement.offsetY;
 
-  if (
-    sEtatJeu == "intro" &&
-    detecterClicObjet(curseurX, curseurY, oBoutonDemarrer) == true //to click on button
-  ) {
-    sEtatJeu = "jeu";
-    oSonMusique.play(); //play sound
-  }
-  if (
-    detecterClicObjet(curseurX, curseurY, oBoutonsReponse[nPositionReponse]) == //to click answers
-    true
-  ) {
-    nPointage += 1;
-    ifProblemeCalled = false;
-  } else if (detecterClicObjet(curseurX, curseurY, oImageBGReponse) == false) {
-    //when didn't click right answer
-    nVie -= 1;
-  }
-  if (
-    detecterClicObjet(curseurX, curseurY, oBoutonDemarrer) == true //restart game after finishing
-  ) {
-    sEtatJeu = "jeu";
-    nVie = 5;
-    nPointage = 0;
-  }
-  // console.log(nPointage, nVie);
+    if (sEtatJeu == "intro" && detecterClicObjet(curseurX, curseurY, oBoutonDemarrer) == true) {
+        sEtatJeu = "jeu";
+        oSonMusique.play(); //play sound
+        melangerSolution();
+    }
+
+    if (sEtatJeu == "jeu" && detecterClicObjet(curseurX, curseurY, oBoutonsReponse[nPositionReponse]) == true) {
+        nPointage += 1;
+        ifProblemeCalled = false;
+    } else if (sEtatJeu == "jeu" && detecterClicObjet(curseurX, curseurY, oImageBGReponse) == false) {
+        //when didn't click right answer
+        nVie -= 1;
+    }
+    if (
+        sEtatJeu == "fin" &&
+        detecterClicObjet(curseurX, curseurY, oBoutonDemarrer) == true //restart game after finishing
+    ) {
+        sEtatJeu = "intro";
+        nVie = 5;
+        nPointage = 0;
+    }
+    // console.log(nPointage, nVie);
 }
 
 //-----------show game
 function afficherIntro() {
-  oContexte.clearRect(0, 0, oCanvasHTML.width, oCanvasHTML.height);
-  //Title
-  oContexte.fillStyle = "#F4B860";
-  oContexte.font = "50px Woodstamp";
-  oContexte.textAlign = "center";
-  oContexte.fillText("Chasse", nLargeur / 2, 150);
+    oContexte.clearRect(0, 0, oCanvasHTML.width, oCanvasHTML.height);
+    //Title
+    oContexte.fillStyle = "#F4B860";
+    oContexte.font = "50px Woodstamp";
+    oContexte.textAlign = "center";
+    oContexte.fillText("Chasse", nLargeur / 2, 150);
 
-  //instructions
-  oContexte.fillStyle = "#242424ff";
-  oContexte.font = "20px Typewriter";
-  oContexte.textAlign = "center";
-  oContexte.fillText(
-    "Trouver la bonne réponse de la question mathématique et clique sur cela.",
-    nLargeur / 2,
-    200
-  );
+    //instructions
+    oContexte.fillStyle = "#242424ff";
+    oContexte.font = "20px Typewriter";
+    oContexte.textAlign = "center";
+    oContexte.fillText("Trouver la bonne réponse de la question mathématique et clique sur cela.", nLargeur / 2, 200);
 
-  //button
-  oBoutonDemarrer.teinte++;
+    //button
+    oBoutonDemarrer.teinte++;
 
-  if (oBoutonDemarrer.teinte >= 360) {
-    oBoutonDemarrer.teinte = 0;
-  }
+    if (oBoutonDemarrer.teinte >= 360) {
+        oBoutonDemarrer.teinte = 0;
+    }
 
-  oContexte.fillStyle = `hsl(${oBoutonDemarrer.teinte}, 50%, 50%)`;
-  oContexte.fillRect(
-    oBoutonDemarrer.x,
-    oBoutonDemarrer.y,
-    oBoutonDemarrer.largeur,
-    oBoutonDemarrer.hauteur
-  );
-  oContexte.fillStyle = "#242424ff";
-  oContexte.fillText(
-    oBoutonDemarrer.texte,
-    oBoutonDemarrer.x + oBoutonDemarrer.largeur / 2,
-    oBoutonDemarrer.y + oBoutonDemarrer.hauteur / 2 + 8
-  );
+    oContexte.fillStyle = `hsl(${oBoutonDemarrer.teinte}, 50%, 50%)`;
+    oContexte.fillRect(oBoutonDemarrer.x, oBoutonDemarrer.y, oBoutonDemarrer.largeur, oBoutonDemarrer.hauteur);
+    oContexte.fillStyle = "#242424ff";
+    oContexte.fillText(
+        oBoutonDemarrer.texte,
+        oBoutonDemarrer.x + oBoutonDemarrer.largeur / 2,
+        oBoutonDemarrer.y + oBoutonDemarrer.hauteur / 2 + 8
+    );
 }
 
 function afficherJeu() {
-  oContexte.drawImage(oImageBG, 0, 0, nLargeur, nHauteur);
+    oContexte.drawImage(oImageBG, 0, 0, nLargeur, nHauteur);
 
-  if (ifProblemeCalled == false) {
-    //to stop equation change infinitely
-    problemeMath();
+    if (ifProblemeCalled == false) {
+        //to stop equation change infinitely
+        problemeMath();
 
-    // console.log(sProblemeMath, nSolution);
-    ifProblemeCalled = true;
-    oContexte.fillText(sProblemeMath, nLargeur / 2, nHauteur - 75);
-    melangerSolution();
-  } else if (ifProblemeCalled == true) {
-    oContexte.fillText(sProblemeMath, nLargeur / 2, nHauteur - 75);
-    afficherMelangerSolution();
-  }
+        // console.log(sProblemeMath, nSolution);
+        ifProblemeCalled = true;
+        oContexte.fillText(sProblemeMath, nLargeur / 2, nHauteur - 75);
+        melangerSolution();
+    } else if (ifProblemeCalled == true) {
+        oContexte.fillText(sProblemeMath, nLargeur / 2, nHauteur - 75);
+        afficherMelangerSolution();
+    }
 
-  if (nVie == 0 || nPointage == 10) {
-    sEtatJeu = "fin";
-  }
-  dessinerUI();
+    if (nVie == 0 || nPointage == 10) {
+        sEtatJeu = "fin";
+    }
+    dessinerUI();
 }
 
-function problemeMath() { //make the mathematical problemes 
-  let nSymboleMathRandom = Math.ceil(Math.random() * 4);
+function problemeMath() {
+    //make the mathematical problemes
+    let nSymboleMathRandom = Math.ceil(Math.random() * 4);
 
-  for (let i = 0; i <= 2; i++) {
-    let nReponse;
-    let sSymbole;
-    let nNumero1 = Math.ceil(Math.random() * 12);
-    let nNumero2 = Math.ceil(Math.random() * 12);
+    for (let i = 0; i <= 2; i++) {
+        let nReponse;
+        let sSymbole;
+        let nNumero1 = Math.ceil(Math.random() * 12);
+        let nNumero2 = Math.ceil(Math.random() * 12);
 
-    if (nSymboleMathRandom == 1) {
-      nReponse = nNumero1 + nNumero2;
-      sSymbole = "+";
-    } else if (nSymboleMathRandom == 2) {
-      nReponse = nNumero1 - nNumero2;
-      sSymbole = "-";
-    } else if (nSymboleMathRandom == 3) {
-      nReponse = nNumero1 * nNumero2;
-      sSymbole = "*";
-    } else if (nSymboleMathRandom == 4) {
-      nReponse = nNumero1 / nNumero2;
-      nReponse = nReponse.toFixed(2);
-      sSymbole = "/";
+        if (nSymboleMathRandom == 1) {
+            nReponse = nNumero1 + nNumero2;
+            sSymbole = "+";
+        } else if (nSymboleMathRandom == 2) {
+            nReponse = nNumero1 - nNumero2;
+            sSymbole = "-";
+        } else if (nSymboleMathRandom == 3) {
+            nReponse = nNumero1 * nNumero2;
+            sSymbole = "*";
+        } else if (nSymboleMathRandom == 4) {
+            nReponse = nNumero1 / nNumero2;
+            nReponse = nReponse.toFixed(2);
+            sSymbole = "/";
+        }
+
+        nReponses[i] = nReponse;
+
+        if (i == 0) {
+            sProblemeMath = `${nNumero1} ${sSymbole} ${nNumero2} = `;
+        }
     }
-
-    nReponses[i] = nReponse;
-
-    if (i == 0) {
-      sProblemeMath = `${nNumero1} ${sSymbole} ${nNumero2} = `;
-    }
-  }
 }
 
-function melangerSolution() {//mix the order of the answers
-  nPositionReponse = Math.ceil(Math.random() * 3) - 1;
+function melangerSolution() {
+    //mix the order of the answers
+    nPositionReponse = Math.ceil(Math.random() * 3) - 1;
 
-  for (let i = 0; i <= 2; i++) {
-    let oBoutonReponse = {
-      x: 60 + i * 270,
-      y: 245,
-      largeur: 100,
-      hauteur: 100,
-      image: new Image(),
-    };
-    oBoutonReponse.image.src = "assets/images/temp-planche.png";
-    if (nPositionReponse == i) {
-      oBoutonReponse.reponse = nReponses[0];
-    } else {
-      let nPositionMauvaiseReponse;
-      if (nReponses.length == 2) {
-        nPositionMauvaiseReponse = 1;
-      } else {
-        nPositionMauvaiseReponse = Math.ceil(Math.random()) + 1;
-      }
+    for (let i = 0; i <= 2; i++) {
+        let oBoutonReponse = {
+            x: 60 + i * 270,
+            y: 245,
+            largeur: 100,
+            hauteur: 100,
+            image: new Image(),
+        };
+        oBoutonReponse.image.src = "assets/images/temp-planche.png";
+        if (nPositionReponse == i) {
+            oBoutonReponse.reponse = nReponses[0];
+        } else {
+            let nPositionMauvaiseReponse;
+            if (nReponses.length == 2) {
+                nPositionMauvaiseReponse = 1;
+            } else {
+                nPositionMauvaiseReponse = Math.ceil(Math.random()) + 1;
+            }
 
-      // console.log(nPositionMauvaiseReponse);
-      // console.log(nReponses);
+            // console.log(nPositionMauvaiseReponse);
+            // console.log(nReponses);
 
-      oBoutonReponse.reponse = nReponses[nPositionMauvaiseReponse];
-      nReponses.splice(nPositionMauvaiseReponse, 1);
+            oBoutonReponse.reponse = nReponses[nPositionMauvaiseReponse];
+            nReponses.splice(nPositionMauvaiseReponse, 1);
+        }
+
+        oBoutonsReponse[i] = oBoutonReponse;
     }
-
-    oBoutonsReponse[i] = oBoutonReponse;
-  }
-  // console.log(oBoutonsReponse);
+    // console.log(oBoutonsReponse);
 }
 
 function afficherMelangerSolution() {
-  for (let i = 0; i < oBoutonsReponse.length; i++) {
-    oContexte.drawImage(
-      oBoutonsReponse[i].image,
-      oBoutonsReponse[i].x,
-      oBoutonsReponse[i].y
-    );
-    oContexte.fillText(
-      `${oBoutonsReponse[i].reponse}`,
-      oBoutonsReponse[i].x + 50,
-      oBoutonsReponse[i].y + oBoutonsReponse[i].hauteur / 2
-    );
-  }
+    for (let i = 0; i < oBoutonsReponse.length; i++) {
+        oContexte.drawImage(oBoutonsReponse[i].image, oBoutonsReponse[i].x, oBoutonsReponse[i].y);
+        oContexte.fillText(
+            `${oBoutonsReponse[i].reponse}`,
+            oBoutonsReponse[i].x + 50,
+            oBoutonsReponse[i].y + oBoutonsReponse[i].hauteur / 2
+        );
+    }
 }
 
 function dessinerUI() {
-  oContexte.fillText(`${nPointage} Pts`, 50, 80);
-  oContexte.fillText(`${nVie} vie`, 50, 50);
-  oContexte.fillStyle = "white";
+    oContexte.fillText(`${nPointage} Pts`, 50, 80);
+    oContexte.fillText(`${nVie} vie`, 50, 50);
+    oContexte.fillStyle = "white";
 }
 
 function afficherFin() {
-  if (nVie <= 0) {
-    oContexte.fillText(
-      "Vous avez perdu... Il y eu des disparaisons des enfants...",
-      nLargeur / 2,
-      nHauteur / 2
-    );
-  } else if (nPointage == 10) {
-    oContexte.fillText(
-      "Vous avez gagné! Vous n'avez plus faim!",
-      nLargeur / 2,
-      nHauteur / 2
-    );
-  }
-  oBoutonDemarrer.teinte++;
+    if (nVie <= 0) {
+        oContexte.fillText("Vous avez perdu... Il y eu des disparitions des enfants...", nLargeur / 2, nHauteur / 2);
+    } else if (nPointage == 10) {
+        oContexte.fillText("Vous avez gagné! Vous n'avez plus faim!", nLargeur / 2, nHauteur / 2);
+    }
+    oBoutonDemarrer.teinte++;
 
-  if (oBoutonDemarrer.teinte >= 360) {
-    oBoutonDemarrer.teinte = 0;
-  }
+    if (oBoutonDemarrer.teinte >= 360) {
+        oBoutonDemarrer.teinte = 0;
+    }
 
-  oContexte.fillStyle = `hsl(${oBoutonDemarrer.teinte}, 50%, 50%)`;
-  oContexte.fillRect(
-    oBoutonDemarrer.x,
-    oBoutonDemarrer.y,
-    oBoutonDemarrer.largeur,
-    oBoutonDemarrer.hauteur
-  );
-  oContexte.fillStyle = "#242424ff";
-  oContexte.fillText(
-    oBoutonDemarrer.texte,
-    oBoutonDemarrer.x + oBoutonDemarrer.largeur / 2,
-    oBoutonDemarrer.y + oBoutonDemarrer.hauteur / 2 + 8
-  );
+    oContexte.fillStyle = `hsl(${oBoutonDemarrer.teinte}, 50%, 50%)`;
+    oContexte.fillRect(oBoutonDemarrer.x, oBoutonDemarrer.y, oBoutonDemarrer.largeur, oBoutonDemarrer.hauteur);
+    oContexte.fillStyle = "#242424ff";
+    oContexte.fillText(
+        oBoutonDemarrer.texte,
+        oBoutonDemarrer.x + oBoutonDemarrer.largeur / 2,
+        oBoutonDemarrer.y + oBoutonDemarrer.hauteur / 2 + 8
+    );
 }
 
 //-------------------------------fonction utilitaires
 function detecterClicObjet(curseurX, curseurY, objet) {
-  if (
-    curseurX >= objet.x &&
-    curseurX <= objet.x + objet.largeur &&
-    curseurY >= objet.y &&
-    curseurY <= objet.y + objet.hauteur
-  ) {
-    return true;
-  }
-  return false;
+    if (curseurX >= objet.x && curseurX <= objet.x + objet.largeur && curseurY >= objet.y && curseurY <= objet.y + objet.hauteur) {
+        return true;
+    }
+    return false;
 }
 
 window.addEventListener("load", initialiser);
